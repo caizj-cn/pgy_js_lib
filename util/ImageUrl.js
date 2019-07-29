fcd4385cdce6918b3a80be91cecfaf84dd54df0e
caizj.cn <caizj_cn@163.com>
2019-07-29 10:32:49 +0800

    update
    update

--- util/ImageUrl.js
+++ util/ImageUrl.js
@@ -1,45 +1,51 @@
-import Log from 'Log';
-
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        spriteImage: cc.Sprite,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        this.checkDefault();
+    },
 
     start () {
 
     },
 
+    checkDefault(){
+        if(this._defaultSpf == null){
+            this._defaultSpf = this.getComponent(cc.Sprite).spriteFrame;
+        }
+    },
 
     /**
-     * 去url上拉取图片（服务端需要做跨域处理）
-     *
-     * @param {*} url
-     * @returns
+     * @description: 获取Url图
+     * @param {string} url 路径
+     * @param {string} type 图片类型，如:png
+     * @return: 
      */
-    loadUrl(url){
-        if(!url.endsWith('.jpg') && !url.endsWith('.png')){
-            Log.error('url error', url);
+    loadUrl(url, type = null){
+        this.checkDefault();
+        
+        if(url == null || url == ''){
+            cc.error('invalid url', url);
+            this.getComponent(cc.Sprite).spriteFrame = this._defaultSpf;
             return;
         }
 
-        cc.loader.load(url, (err, texture) => {
+        cc.loader.load(typeof type == 'string'? {url: url, type: type}: url, (err, texture) => {
             if(err != null){
-                Log.error(url, err);
+                cc.error(url, err);
                 return;
             }
 
-            let width = this.spriteImage.node.width;
-            let height = this.spriteImage.node.height;
-
-            this.spriteImage.spriteFrame = new cc.SpriteFrame(texture);
-            this.spriteImage.node.width = width;
-            this.spriteImage.node.height = height;
+            // 根据默认大小显示
+            this.getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(texture);
+            let size = this._defaultSpf.getOriginalSize();
+            this.node.width = size.width;
+            this.node.height = size.height;
         });
     },
 
